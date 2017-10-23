58d1926642bf91b1baf97b04b63ecdf8d17c46fc
itmifen <lihjay@163.com>
2017-10-23 16:35:07 +0800

    测试框架修改

--- src/test/com/itmifen/utility/AnnotationTest.java
+++ src/test/com/itmifen/utility/AnnotationTest.java
@@ -3,20 +3,39 @@ package com.itmifen.utility;
 import com.itmifen.utility.annotation.entityvalid.EntityCheckUtil;
 import com.itmifen.utility.annotation.entityvalid.Valid;
 import com.itmifen.utility.annotation.entityvalid.ValidResultEntity;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 
 
 public class AnnotationTest {
 
+    private  TestEntity testEntity=new TestEntity();
+
+    @Before
+    public  void createEntity() {
+        this.testEntity.setImages("from runoob.com");
+        this.testEntity.setTitle("aa");
+    }
+
     @Test
     public void testValidate() throws Exception{
-        TestEntity testEntity=new TestEntity();
-        testEntity.setImages("from runoob.com");
-        testEntity.setTitle("aa");
        ValidResultEntity validResultEntity = EntityCheckUtil.validate(testEntity);
        System.out.println(validResultEntity.getMessage());
        assert  validResultEntity.isSucceed();
     }
+
+    /**
+     * 原始的验证思路
+     */
+    @Test
+        public void  testOldMethod(){
+            if(testEntity.getImages().length()>2){
+                //这里是业务逻辑
+            }
+            if(testEntity.getTitle().length()>2){
+                //这里是业务逻辑
+            }
+        }
 
     /**
      * 定义测试的实体
